2f13a3bdeaf54227d30cbddb9fb59c5dcfdcc454
KarlaSouza1 <127553925+KarlaSouza1@users.noreply.github.com>
2023-08-15 20:13:08 -0300

    Add files via upload

---
+++ Aula01/aula01.java
@@ -0,0 +1,13 @@
+package Aula01;
+/**
+ * Exercício 01
+ * Aula:10/07/2023
+ * Disciplina: Linguagem de Programação
+ * @author Karla Souza
+ */
+public class aula01 {
+    public static void main(String[] args) {
+        System.out.println("Ola Mundo");
+    }
+    
+}
