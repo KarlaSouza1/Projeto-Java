964856d7c6298d01241d972a72f2a4699936c69c
KarlaSouza1 <127553925+KarlaSouza1@users.noreply.github.com>
2023-08-15 20:14:29 -0300

    Add files via upload

---
+++ Aula06/aula06.java
@@ -0,0 +1,25 @@
+package Aula06;
+
+/**
+ * Exercicio 06
+ * 
+ * Aula: 15/08/2023
+ * @author Karla Souza
+ */
+public class aula06 {
+    public static void main(String[] args) {
+        float nota1 = 8;
+        float nota2 = 5;
+        float nota3 = 10;
+        float nota4 = 7;
+        
+        float soma, media = 0;
+        soma = nota1 + nota2 + nota3 + nota4;
+        media = soma / 4;
+        System.out.println("Soma: " + soma);
+        System.out.println("Media: " + media);
+        
+             
+    }
+    
+}
