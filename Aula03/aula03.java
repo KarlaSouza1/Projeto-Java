64f261b1bd482f6660f839ce444b242267a9a1b7
KarlaSouza1 <127553925+KarlaSouza1@users.noreply.github.com>
2023-08-15 20:13:42 -0300

    Add files via upload

---
+++ Aula03/aula03.java
@@ -0,0 +1,19 @@
+
+package Aula03;
+
+/**
+ * Exercício 03
+ * Aula:01/08/2023
+ * Disciplina: Linguagem de Programação
+ * Materia: Git e GitHub
+ * @author Karla Souza
+ */
+public class aula03 {
+    public static void main(String[] args) {
+        int meuNum = 5;               // Inteiros
+        float meuNumReal = 5.99f;    // Reais
+        char meuCaracter = 'D';         // Caracter
+        boolean meuLogico = true;       // Lógico
+        String meuTexto = "Hello";     // Texto
+    }
+}
