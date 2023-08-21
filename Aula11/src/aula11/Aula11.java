73f4b62a8e802285240bdd55222e4fd3b0ab5c82
KarlaSouza1 <127553925+KarlaSouza1@users.noreply.github.com>
2023-08-21 20:09:31 -0300

    Add files via upload

---
+++ Aula11/src/aula11/Aula11.java
@@ -0,0 +1,25 @@
+
+package aula11;
+
+/**
+ * Aula 11
+ * Disciplina: Linguagem de programação
+ * Data: 21/08/2023
+ * @author Karla Souza
+ */
+public class Aula11 {
+    public static void main(String[] args) {
+        int contador = 0;
+        while (contador < 10){
+            System.out.println(contador);
+            contador++;
+                    
+        }
+        
+        for (int i = 0; i < 10; i++){
+            System.out.println(i);
+        }
+                          
+     }
+    
+}
