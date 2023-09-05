989601fef8b5d3d02155af20e45ae8cbd2c13b05
KarlaSouza1 <127553925+KarlaSouza1@users.noreply.github.com>
2023-09-05 20:28:23 -0300

    Add files via upload

---
+++ Aula16/src/aula16/Usuario.java
@@ -0,0 +1,32 @@
+
+package aula16;
+
+/**
+ * LInguagem de Programação
+ * Tela Login
+ * 29/08/2023
+ * @author 16343359613
+ */
+public class Usuario {
+        private String nome = "Karla";
+        private String senha = "12345";
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+        
+    }
+    
+
