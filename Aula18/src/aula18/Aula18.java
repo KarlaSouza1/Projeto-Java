319e3bf88430428291cedce440cebc6681424e93
KarlaSouza1 <127553925+KarlaSouza1@users.noreply.github.com>
2023-09-05 20:28:56 -0300

    Add files via upload

---
+++ Aula18/src/aula18/Aula18.java
@@ -0,0 +1,20 @@
+
+package aula18;
+
+/**
+ *
+ * @author 16343359613
+ */
+public class Aula18 {
+    public static void main(String[] args) {
+      int incr = 1;
+      int decr = 15;
+      
+      for(int i =0; i<15;i++){
+          System.out.println(incr + "/"+ decr);
+          incr++;
+          decr--;
+      }
+    }
+    
+}
